f1f00039579cf324a39bfb088f77359a089f89db
Serhiy Yavnyi <blacktaxi@gmail.com>
2025-03-30 19:29:41 -0700

    Formatting

--- perftest/all.lua
+++ perftest/all.lua
@@ -1,118 +1,188 @@
-class = require 'oops'
-local _ = require 'moses'
-require 'bench'
+class = require("oops")
+local _ = require("moses")
+require("bench")
 
 local benchmarks = {
-  { name = 'Class definition', action = function ()
-    return function ()
-      local C = class { }
-    end
-  end},
-  { name = '1-inheritance class definition', action = function ()
-    local C = class { }
-    return function ()
-      local C1 = class(C) { }
-    end
-  end},
-  { name = '3-inheritance class definition', action = function ()
-    local C = class { }
-    C = class(C) { }
-    C = class(C) { }
-    C = class(C) { }
-    return function ()
-      local C1 = class(C) { }
-    end
-  end},
-  { name = 'Instance creation', action = function ()
-    local C1 = class { }
-    return function ()
-      local o = C1()
-    end
-  end},
-  { name = '1-inheritance instance creation', action = function ()
-    local C2 = class { }
-    local C3 = class(C2) { }
-    return function ()
-      local o = C3()
-    end
-  end},
-  { name = '3-inheritance instance creation', action = function ()
-    local C2 = class { }
-    local C3 = class(C2) { }
-    local C4 = class(C3) { }
-    local C5 = class(C4) { }
-    return function ()
-      local o = C5()
-    end
-  end},
-  { name = 'Complex instance creation', action = function ()
-    local C2 = class { x = 5, y = 6, z = function (self) return 9 end }
-    return function ()
-      local o = C2()
-    end
-  end},
-  { name = 'Instance method call', action = function ()
-    local C2 = class { x = 5, y = 6, z = function (self) return 9 end }
-    local o = C2()
-    return function ()
-      o:z()
-    end
-  end},
-  { name = '1-inheritance instance method call', action = function ()
-    local C1 = class { x = 5, y = 6, z = function (self) return 9 end }
-    local C2 = class(C1) { q = function (self) return 10 end }
-    local o = C2()
-    return function ()
-      o:z()
-    end
-  end},
-  { name = 'Instance mutation via method call', action = function ()
-    local C = class { x = 0, method = function (self) self.x = self.x + 1 end }
-    local o = C()
-    return function ()
-      o:method()
-    end
-  end},
-  { name = 'Is-class test', action = function ()
-    local C = class { }
-    return function ()
-      class.isclass(C)
-    end
-  end},
-  { name = 'Is-object test', action = function ()
-    local C = class { }
-    local o = C()
-    return function ()
-      class.isobject(o)
-    end
-  end},
-  { name = 'Is-instance-of test', action = function ()
-    local C = class { }
-    local o = C()
-    return function ()
-      class.isinstanceof(o, C)
-    end
-  end},
-  { name = '3-inheritance is-instance-of test', action = function ()
-    local C = class { }
-    local C1 = class(C) { }
-    local C2 = class(C1) { }
-    local C3 = class(C2) { }
-    local o = C3()
-    return function ()
-      class.isinstanceof(o, C)
-    end
-  end},
+	{
+		name = "Class definition",
+		action = function()
+			return function()
+				local C = class({})
+			end
+		end,
+	},
+	{
+		name = "1-inheritance class definition",
+		action = function()
+			local C = class({})
+			return function()
+				local C1 = class(C)({})
+			end
+		end,
+	},
+	{
+		name = "3-inheritance class definition",
+		action = function()
+			local C = class({})
+			C = class(C)({})
+			C = class(C)({})
+			C = class(C)({})
+			return function()
+				local C1 = class(C)({})
+			end
+		end,
+	},
+	{
+		name = "Instance creation",
+		action = function()
+			local C1 = class({})
+			return function()
+				local o = C1()
+			end
+		end,
+	},
+	{
+		name = "1-inheritance instance creation",
+		action = function()
+			local C2 = class({})
+			local C3 = class(C2)({})
+			return function()
+				local o = C3()
+			end
+		end,
+	},
+	{
+		name = "3-inheritance instance creation",
+		action = function()
+			local C2 = class({})
+			local C3 = class(C2)({})
+			local C4 = class(C3)({})
+			local C5 = class(C4)({})
+			return function()
+				local o = C5()
+			end
+		end,
+	},
+	{
+		name = "Complex instance creation",
+		action = function()
+			local C2 = class({
+				x = 5,
+				y = 6,
+				z = function(self)
+					return 9
+				end,
+			})
+			return function()
+				local o = C2()
+			end
+		end,
+	},
+	{
+		name = "Instance method call",
+		action = function()
+			local C2 = class({
+				x = 5,
+				y = 6,
+				z = function(self)
+					return 9
+				end,
+			})
+			local o = C2()
+			return function()
+				o:z()
+			end
+		end,
+	},
+	{
+		name = "1-inheritance instance method call",
+		action = function()
+			local C1 = class({
+				x = 5,
+				y = 6,
+				z = function(self)
+					return 9
+				end,
+			})
+			local C2 = class(C1)({
+				q = function(self)
+					return 10
+				end,
+			})
+			local o = C2()
+			return function()
+				o:z()
+			end
+		end,
+	},
+	{
+		name = "Instance mutation via method call",
+		action = function()
+			local C = class({
+				x = 0,
+				method = function(self)
+					self.x = self.x + 1
+				end,
+			})
+			local o = C()
+			return function()
+				o:method()
+			end
+		end,
+	},
+	{
+		name = "Is-class test",
+		action = function()
+			local C = class({})
+			return function()
+				class.isclass(C)
+			end
+		end,
+	},
+	{
+		name = "Is-object test",
+		action = function()
+			local C = class({})
+			local o = C()
+			return function()
+				class.isobject(o)
+			end
+		end,
+	},
+	{
+		name = "Is-instance-of test",
+		action = function()
+			local C = class({})
+			local o = C()
+			return function()
+				class.isinstanceof(o, C)
+			end
+		end,
+	},
+	{
+		name = "3-inheritance is-instance-of test",
+		action = function()
+			local C = class({})
+			local C1 = class(C)({})
+			local C2 = class(C1)({})
+			local C3 = class(C2)({})
+			local o = C3()
+			return function()
+				class.isinstanceof(o, C)
+			end
+		end,
+	},
 }
 
-local print_results = function (results)
-  for _, r in ipairs(results) do
-    b, t, m = unpack(r)
-    print(b.name .. ': \t\t\t' .. t * 1000000  .. ' us, \t\t' .. m * 1000 .. ' Kb/1000')
-  end
+local print_results = function(results)
+	for _, r in ipairs(results) do
+		b, t, m = unpack(r)
+		print(b.name .. ": \t\t\t" .. t * 1000000 .. " us, \t\t" .. m * 1000 .. " Kb/1000")
+	end
 end
 
-local results = _.map(benchmarks, function (i, x) return bench(x) end)
+local results = _.map(benchmarks, function(i, x)
+	return bench(x)
+end)
 
 print_results(results)
-
--- perftest/bench.lua
+++ perftest/bench.lua
@@ -1,64 +1,66 @@
-local _ = require 'moses'
+local _ = require("moses")
 
 local function time(action)
-  local startTime = os.clock()
-  action()
-  local endTime = os.clock()  
-  return endTime - startTime
+	local startTime = os.clock()
+	action()
+	local endTime = os.clock()
+	return endTime - startTime
 end
 
 local function sum(list)
-  return _.reduce(list, function (a, b) return a + b end, 0)
+	return _.reduce(list, function(a, b)
+		return a + b
+	end, 0)
 end
 
 local function average(list)
-  return sum(list) / _.count(list)
+	return sum(list) / _.count(list)
 end
 
 function bench(benchmark)
-  print('Timing ' .. benchmark.name .. '...')
+	print("Timing " .. benchmark.name .. "...")
 
-  local min_iterations = benchmark.iterations or 100000
-  local action = benchmark.action()
+	local min_iterations = benchmark.iterations or 100000
+	local action = benchmark.action()
 
-  -- a function that times an action 'properly'
-  local proper_time = function (action, min_time)
-    -- minimal benchmark time
-    local min_time = min_time or 1.0
-    local total_time = 0.0
-    local total_iterations = 0
-    collectgarbage()
-    local start_mem = collectgarbage('count')
-    collectgarbage('stop')
-    -- iterate until total time is >= minimal time
-    repeat
-      total_time = total_time + time(function ()
-        for _ = 0, min_iterations do
-          action()
-        end
-      end)
-      total_iterations = total_iterations + min_iterations
-    until total_time >= min_time
-    local end_mem = collectgarbage('count')
-    collectgarbage('restart')
-    print(total_iterations, total_time)
-    return total_time, total_iterations, (end_mem - start_mem)
-  end
+	-- a function that times an action 'properly'
+	local proper_time = function(action, min_time)
+		-- minimal benchmark time
+		local min_time = min_time or 1.0
+		local total_time = 0.0
+		local total_iterations = 0
+		collectgarbage()
+		local start_mem = collectgarbage("count")
+		collectgarbage("stop")
+		-- iterate until total time is >= minimal time
+		repeat
+			total_time = total_time + time(function()
+				for _ = 0, min_iterations do
+					action()
+				end
+			end)
+			total_iterations = total_iterations + min_iterations
+		until total_time >= min_time
+		local end_mem = collectgarbage("count")
+		collectgarbage("restart")
+		print(total_iterations, total_time)
+		return total_time, total_iterations, (end_mem - start_mem)
+	end
 
-  -- measure overhead
-  local overhead_time, overhead_iterations, overhead_mem = proper_time(function () end)
-  local overhead_tpi = overhead_time / overhead_iterations
-  local overhead_mpi = overhead_mem / overhead_iterations
-  -- measure actual benchmark
-  local full_time, full_iterations, full_mem = proper_time(action)
-  local full_tpi = full_time / full_iterations
-  local full_mpi = full_mem / full_iterations
+	-- measure overhead
+	local overhead_time, overhead_iterations, overhead_mem = proper_time(function() end)
+	local overhead_tpi = overhead_time / overhead_iterations
+	local overhead_mpi = overhead_mem / overhead_iterations
+	-- measure actual benchmark
+	local full_time, full_iterations, full_mem = proper_time(action)
+	local full_tpi = full_time / full_iterations
+	local full_mpi = full_mem / full_iterations
 
-  print('Finished.')
+	print("Finished.")
 
-  return { benchmark, full_tpi - overhead_tpi, full_mpi - overhead_mpi }
+	return { benchmark, full_tpi - overhead_tpi, full_mpi - overhead_mpi }
 end
 
 return {
-  bench
+	bench,
 }
